7ef0051fffdb45ddfada6f2a9ce3015d98505ccc
Luiz Junior <luiz.junior@blueticket.com.br>
2022-05-18 23:05:40 -0300

    sequelize configured; User routes added (create, update); Session store with JWT added; Database configured/

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database'
+
+import User from '../app/models/User'
+
+const models = [User]
+
+class Database {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.conection = new Sequelize(databaseConfig)
+
+    models.forEach(model => model.init(this.conection))
+  }
+}
+
+export default new Database()
